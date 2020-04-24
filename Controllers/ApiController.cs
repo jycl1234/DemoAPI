9c5c3f21f6689e270820bfdf9d568da5e81d9ecb
Jeffrey Yang <jycl1234@users.noreply.github.com>
2020-04-24 12:55:47 -0700

    switched from hardcoded to database

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
 
 namespace DemoAPI.Controllers
 {
@@ -8,15 +12,53 @@ namespace DemoAPI.Controllers
     public class ApiController : Controller
     {
 
-        private static readonly List<Item> items = new List<Item>() {
-            new Item { Id = 1, ItemName = "ITEM 1", Cost = 100},
-            new Item { Id = 2, ItemName = "ITEM 2", Cost = 200},
-            new Item { Id = 3, ItemName = "ITEM 1", Cost = 250},
-            new Item { Id = 4, ItemName = "ITEM 3", Cost = 300},
-            new Item { Id = 5, ItemName = "ITEM 4", Cost = 50},
-            new Item { Id = 6, ItemName = "ITEM 5", Cost = 40},
-            new Item { Id = 7, ItemName = "ITEM 2", Cost = 200}
-        };
+        // placeholder - this is only for reference for the first iteration and expected shape of the table
+
+        //private static readonly List<Item> items = new List<Item>() {
+        //    new Item { Id = 1, ItemName = "ITEM 1", Cost = 100},
+        //    new Item { Id = 2, ItemName = "ITEM 2", Cost = 200},
+        //    new Item { Id = 3, ItemName = "ITEM 1", Cost = 250},
+        //    new Item { Id = 4, ItemName = "ITEM 3", Cost = 300},
+        //    new Item { Id = 5, ItemName = "ITEM 4", Cost = 50},
+        //    new Item { Id = 6, ItemName = "ITEM 5", Cost = 40},
+        //    new Item { Id = 7, ItemName = "ITEM 2", Cost = 200}
+        //};
+
+
+        // this whole block is obviously highly insecure, especially when committing to a public repo. i've added a constants class and put it on .gitignore, but ideally
+        // this would be handled better in a real environment, either through a separate class library or what not
+        private static List<Item> FetchData()
+        {
+            List<Item> items = new List<Item>();
+            MySqlConnection conn = new MySqlConnection(Constants.connString);
+            string cmd = "SELECT * FROM demoapi.items";
+
+            try
+            {
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                foreach (DataRow row in dt.Rows)
+                {
+                    Item item = new Item { Id = Convert.ToInt64(row["ID"]), ItemName = row["ITEM_NAME"].ToString(), Cost = Convert.ToInt64(row["COST"]) };
+                    items.Add(item);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.ToString());
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+            return items;
+        }
+
+        // i'm using attribute routing here for a few reasons:
+        // 1. simplicity. this is a demo/poc, so getting something up and running was priority
+        // 2. conciseness. there is no ambiguity about the expected paths
+        // in a full project with more endpoints, these would be located in webapiconfig instead
 
         // base - shows all records
 
@@ -24,6 +66,7 @@ namespace DemoAPI.Controllers
         [Route("[controller]")]
         public List<Item> Get()
         {
+            List<Item> items = FetchData();
             try
             {
                 return items;
@@ -37,9 +80,10 @@ namespace DemoAPI.Controllers
         // not requested -- shows item by id
 
         [HttpGet("{id}")]
-        [Route("[controller]/{id}")]
-        public Item Get(int id)
+        [Route("[controller]/getitem/{id}")]
+        public Item GetItem(int id)
         {
+            List<Item> items = FetchData();
             try
             {
                 return items.Where(x => x.Id == id).FirstOrDefault();
@@ -56,9 +100,17 @@ namespace DemoAPI.Controllers
         [Route("[controller]/getmaxprices")]
         public List<Item> GetMaxPrices()
         {
+            List<Item> items = FetchData();
             try
             {
-                return null;
+                List<string> keys = items.Select(x => x.ItemName).Distinct().ToList() as List<string>;
+                List<Item> sortedList = items.OrderBy(x => x.ItemName).ThenByDescending(x => x.Cost).ToList() as List<Item>;
+                List<Item> itemMaxPrices = new List<Item>();
+                foreach (string key in keys)
+                {
+                    itemMaxPrices.Add(sortedList.Where(x => x.ItemName == key).FirstOrDefault());
+                }
+                return itemMaxPrices;
             }
             catch
             {
@@ -72,6 +124,7 @@ namespace DemoAPI.Controllers
         [Route("[controller]/getmaxprice/{itemname}")]
         public long GetMaxPrice(string itemname)
         {
+            List<Item> items = FetchData();
             try
             {
                 List<Item> itemsWithName = items.FindAll(x => x.ItemName == itemname);
